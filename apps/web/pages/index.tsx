f07ffec657e35e9b721625ac9602d56a76466d80
CJ <corlouch@gmail.com>
2023-02-28 08:29:09 +0000

    added adonis project in apps

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,6 +1,12 @@
 import { Button } from "ui";
 
 export default function Web() {
+  const fetchHello = async () => {
+    const response = await fetch("/");
+    const data = await response.json();
+    alert(data);
+  };
+
   return (
     <div>
       <h1>Web</h1>
